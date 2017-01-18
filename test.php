c97bda04841d5ff61eb44f2d3003470f8ef46d34
Piotr Lipski <piotr.lipski@bbc.co.uk>
2017-01-18 11:53:29 +0000

    Add two solutions for Codility

--- test.php
+++ test.php
@@ -1,7 +1,72 @@
 <?php
+/**
+ * N and K are integers within the range [0..100];
+ * each element of array A is an integer within the range [−1,000..1,000].
+ * @param $A
+ * @param $K
+ * @return array
+ */
+function cyclicRotation($A, $K)
+{
 
-// Binary Gap
-function solution($N)
+    $count = count($A);
+
+    if ($K == 0 || $count <= 1) {
+        return $A;
+    }
+
+    if ($K > $count) {
+        $K = $K % $count;
+    }
+    $B = [];
+    for ($i = 0; $i < $count; $i++) {
+
+        $nK = ($i + $K) % $count;
+        $B[$nK] = $A[$i];
+    }
+    return $B;
+
+}
+
+//var_dump(cyclicRotation([2,4,7,11], 1));
+
+/**
+ * @param $A
+ * @return int|string
+ */
+function oddOccurrencesInArray($A)
+{
+    if (count($A) == 1) {
+        return $A[0];
+    }
+    $count = count($A);
+    $B     = [];
+
+    for ($i = 0; $i < $count; $i++) {
+        if (isset($B[$A[$i]])) {
+            unset($B[$A[$i]]);
+        } else {
+            $B[$A[$i]] = true;
+        }
+    }
+    $C = array_diff($B, [false]);
+    foreach ($C as $key => $value) {
+        return $key;
+    }
+
+    return 'Not found';
+}
+
+
+//echo oddOccurrencesInArray([4, 11, 23, 23, 11]);//11, 23, 23, 11
+
+/**
+ * binaryGap
+ *
+ * @param $N
+ * @return int
+ */
+function binaryGap($N)
 {
     $a         = decbin($N);
     $start     = false;
@@ -37,7 +102,7 @@ function solution($N)
 }
 
 
-$c = getrandmax();
-for ($b = getrandmax(); $b > $c - 100; $b--) {
-    solution($b);
-}
+//$c = getrandmax();
+//for ($b = getrandmax(); $b > $c - 100; $b--) {
+//    solution($b);
+//}
